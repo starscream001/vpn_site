871e713d8d05d1cb4fb69b7378d563c1fa7b7767
manik <manik1990@list.ru>
2024-02-08 01:26:50 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function telegram() {
+    window.open("https://t.me/+Yy6TLomnyMhlMDcy", "_blank");
+}
+function whatsapp() {
+    window.open("https://chat.whatsapp.com/GoehxKDEcrX8NNrDUXTszG", "_blank");
+}
